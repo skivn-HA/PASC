2c7a77080d7dc3813c620467f69347aa23808427
skivn-HA <d.bailey1@gmail.com>
2026-06-28 21:30:12 +1000

    Initial PASC upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'pasc-v1';
+
+const PRECACHE_ASSETS = [
+  './index.html',
+  './PASCv2026.06.28.html',
+  './manifest.webmanifest',
+  './icons/icon.svg',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const networkFetch = fetch(event.request)
+        .then((response) => {
+          if (response.ok && (event.request.url.startsWith(self.location.origin) || event.request.url.includes('cdnjs.cloudflare.com'))) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          }
+          return response;
+        })
+        .catch(() => cached);
+
+      return cached || networkFetch;
+    })
+  );
+});
